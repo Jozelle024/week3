d05d6c863c621148bd8670422e85d29a0b94af71
Jozelle <jozellelauzon024@gmail.com>
2018-02-26 12:05:22 +0100

    controllo get giorno

--- es6/Day1/libreriaStudenti.js
+++ es6/Day1/libreriaStudenti.js
@@ -5,7 +5,21 @@ const arrayMesi = ['gennaio','febrraio','marzo', 'aprile','maggio','giugno','lug
 const arraySettimana = ['lunedi', 'martedi','mercoledi','giovedi','venerdi','sabato','domenica'];
 
 const getMese = n =>{return arrayMesi[n];};
-const getGiorno = g => {return arraySettimana[g];};
+const getGiorno = g => {
+
+    const indice = parseInt(n);
+    const indiceIsNan = isNaN(indice);
+    if(indiceIsNan){
+        return `L'indice inserito non è un numero`;
+    }else {
+        if(indice > 0 && indice < 7){
+            return arraySettimana[g];
+        } else {
+            return `L'indice inserito è sbagliato`;
+        }
+    }
+};
+    
 const getMesi = () => {return arrayMesi;};
 const getSettimane = () =>{return arraySettimana;};
 
