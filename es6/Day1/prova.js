cf6bab088830f9e91b987d43fcac1264489cf1e7
Jozelle <jozellelauzon024@gmail.com>
2018-02-26 09:59:13 +0100

    prova con let e differenza con var

---
+++ es6/Day1/prova.js
@@ -0,0 +1,47 @@
+/*jshint esversion: 6 */
+
+for(var i = 0; i < 5; i ++){
+    setTimeout(function(){
+        console.log(i);
+    }, 1000);
+}
+
+for(let j = 0; j < 5; j++){
+    setTimeout(function(){
+        console.log(j);
+    }, 1000);
+}
+
+let vero = true;
+if(vero) {
+    let esempio = 'a';
+}
+//console.log(esempio); //generate error because esempio is declared inside the block
+
+let read, write;
+{
+    let data = {};
+
+    write = function(chiave,valore){
+        data[chiave] = valore;
+    };
+
+    read = function(key){
+        return data [key];
+    };
+}
+
+write('ES6', 'Benvenuti in ES6');
+let messaggio = read('ES6'); 
+console.log(messaggio);
+//console.log(data); // generate error because data is declared inside the block {}
+
+let num = 10;
+function getNum(){
+    if(!num){
+        let num = 1;
+    }
+    return num;
+}
+
+console.log(getNum());
