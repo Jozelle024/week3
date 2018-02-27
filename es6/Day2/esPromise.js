52d039cf35c3ebabda1ada88b72d37296c3dcc4d
Jozelle <jozellelauzon024@gmail.com>
2018-02-27 09:38:24 +0100

    esercitazioni con Promise

---
+++ es6/Day2/esPromise.js
@@ -0,0 +1,8 @@
+/*jshint esversion:6 */
+const libreriaMoment = require('./node_modules/moment/min/moment-with-locales.js');
+
+const promessa = new Promise(
+    function(resolve, reject){
+        
+    }
+);
