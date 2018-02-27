197d879a204075e544445b50c0b9b7602dd22b13
Jozelle <jozellelauzon024@gmail.com>
2018-02-27 15:11:26 +0100

    aggiunti altri task gulp

--- css/Day2/gulpfile.js
+++ css/Day2/gulpfile.js
@@ -3,7 +3,16 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const gutil = require('gulp-util');
 
+gulp.task('default', function(){ //task creation, (name of task, function)
+    return gutil.log('Gulp in esecuzione'); // to execute on terminal -> gulp default
 
+});
+gulp.task('copiaFile', function(){
+    //copy any html files in source / to public/
+    gulp.src('./html*.html').pipe(gulp.dest('dist'));
+    gulp.src('./javascript/*.js').pipe(gulp.dest('dist'));
+    gulp.src('./css/*.css').pipe(gulp.dest('dist'));
+});
 gulp.task('sass', function(){
     return gulp.src('./sass/*.css')
         .pipe(sass().on('error', sass.logError))
